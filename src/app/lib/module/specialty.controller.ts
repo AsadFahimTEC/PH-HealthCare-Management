72c4eadd5e32bcb982fea2fc1c4dc88bdc12462c
AsadFahimTEC <fah485434@gmail.com>
2026-02-05 21:48:47 +0600

    completing specialties api

--- src/app/lib/module/specialty.controller.ts
+++ src/app/lib/module/specialty.controller.ts
@@ -1,15 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express';
 import { SpecialtyService } from './specialty/specialty.service';
+
 const createSpecialty = async (req: Request, res: Response) => {
-    const payload = req.body;
-    const result = await SpecialtyService.createSpecialty(payload);
-    res.status(201).json({
-        success: true,
-        message: "Specialty created successfully",
-        data: result
-    });
+    try {
+        const payload = req.body;
+        const result = await SpecialtyService.createSpecialty(payload);
+        res.status(201).json({
+            success: true,
+            message: "Specialty created successfully",
+            data: result
+        });
+    } catch (error: any) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Failed to create specialty",
+            error: error.message
+        })
+    }
+}
+const getAllSpecialties = async (req: Request, res: Response) => {
+    try {
+        const specialites = await SpecialtyService.getAllSpecialties();
+        res.status(201).json({
+            success: true,
+            message: "Specialty fetched successfully",
+            data: specialites
+        });
+    } catch (error: any) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch specialties",
+            error: error.message
+        })
+    }
+}
+
+const deleteSpecialty = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const result = await SpecialtyService.deleteSpecialty(id as string);
+        res.status(201).json({
+            success: true,
+            message: "Specialty created successfully",
+            data: result
+        });
+    } catch (error: any) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Failed to create specialty",
+            error: error.message
+        })
+    }
 }
 
 export const SpecialtyController = {
-    createSpecialty
+    createSpecialty,
+    getAllSpecialties,
+    deleteSpecialty
 }
