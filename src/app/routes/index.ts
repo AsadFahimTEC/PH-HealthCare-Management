43fb5ee85fa7510e05706b48329c6bbea86db152
AsadFahimTEC <fah485434@gmail.com>
2026-02-07 11:28:43 +0600

    create doctor specialty model and business logic control with transaction

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { SpecialtyRoutes } from "../lib/module/specialty/specialty.route";
 import { AuthRoutes } from "../lib/module/auth/auth.route";
+import { UserRoutes } from "../lib/module/user/user.route";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoutes);
+router.use("/doctors", UserRoutes);
 
 export const IndexRoutes = router;
