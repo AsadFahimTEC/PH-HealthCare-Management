ea56146a0583f04a57341b52eab5f7510eb79470
AsadFahimTEC <fah485434@gmail.com>
2026-02-10 13:16:41 +0600

    creating email sender with nodemailer and google cloud

--- src/config/env.ts
+++ src/config/env.ts
@@ -15,10 +15,44 @@ interface EnvConfig {
     ACCESS_TOKEN_EXPIRES_IN: string,
     REFRESH_TOKEN_EXPIRES_IN: string,
     BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: string,
-    BETTER_AUTH_SESSION_TOKEN_UPDATE_EXPIRES_IN: string
+    BETTER_AUTH_SESSION_TOKEN_UPDATE_EXPIRES_IN: string,
+    EMAIL_SENDER: {
+        SMTP_USER: string;
+        SMTP_PASS: string;
+        SMTP_HOST: string;
+        SMTP_PORT: string;
+        SMTP_FROM: string;
+    }
 }
 
 const loadEnvVariables = (): EnvConfig => {
+
+    const requireEnvVariable = [
+        'NODE_ENV',
+        'PORT',
+        'DATABASE_URL',
+        'BETTER_AUTH_SECRET',
+        'BETTER_AUTH_URL',
+        'ACCESS_TOKEN_SECRET',
+        'REFRESH_TOKEN_SECRET',
+        'ACCESS_TOKEN_EXPIRES_IN',
+        'REFRESH_TOKEN_EXPIRES_IN',
+        'BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN',
+        'BETTER_AUTH_SESSION_TOKEN_UPDATE_EXPIRES_IN',
+        'EMAIL_SENDER_SMTP_USER',
+        'EMAIL_SENDER_SMTP_PASS',
+        'EMAIL_SENDER_SMTP_HOST',
+        'EMAIL_SENDER_SMTP_PORT',
+        'EMAIL_SENDER_SMTP_FROM',
+    ]
+
+    requireEnvVariable.forEach((variable) => {
+        if (!process.env[variable]) {
+            // throw new Error(`Environment variable ${variable} is required but not set in .env file.`);
+            throw new AppError(status.INTERNAL_SERVER_ERROR, `Environment variable ${variable} is required but not set in .env file.`);
+        }
+    })
+
     return {
         NODE_ENV: process.env.NODE_ENV as string,
         PORT: process.env.PORT as string,
@@ -30,30 +64,19 @@ const loadEnvVariables = (): EnvConfig => {
         ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
         REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
         BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: process.env.BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN as string,
-        BETTER_AUTH_SESSION_TOKEN_UPDATE_EXPIRES_IN: process.env.BETTER_AUTH_SESSION_TOKEN_UPDATE_EXPIRES_IN as string
+        BETTER_AUTH_SESSION_TOKEN_UPDATE_EXPIRES_IN: process.env.BETTER_AUTH_SESSION_TOKEN_UPDATE_EXPIRES_IN as string,
+        EMAIL_SENDER: {
+            SMTP_USER: process.env.EMAIL_SENDER_SMTP_USER as string,
+            SMTP_PASS: process.env.EMAIL_SENDER_SMTP_PASS as string,
+            SMTP_HOST: process.env.EMAIL_SENDER_SMTP_HOST as string,
+            SMTP_PORT: process.env.EMAIL_SENDER_SMTP_PORT as string,
+            SMTP_FROM: process.env.EMAIL_SENDER_SMTP_FROM as string,
+        },
+
     }
 }
 
-const requireEnvVariable = [
-    'NODE_ENV',
-    'PORT',
-    'DATABASE_URL',
-    'BETTER_AUTH_SECRET',
-    'BETTER_AUTH_URL',
-    'ACCESS_TOKEN_SECRET',
-    'REFRESH_TOKEN_SECRET',
-    'ACCESS_TOKEN_EXPIRES_IN',
-    'REFRESH_TOKEN_EXPIRES_IN',
-    'BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN',
-    'BETTER_AUTH_SESSION_TOKEN_UPDATE_EXPIRES_IN'
-]
 
-requireEnvVariable.forEach((variable) => {
-    if (!process.env[variable]) {
-        // throw new Error(`Environment variable ${variable} is required but not set in .env file.`);
-        throw new AppError(status.INTERNAL_SERVER_ERROR, `Environment variable ${variable} is required but not set in .env file.`);
-    }
-})
 
 export const envVars = loadEnvVariables();
 
