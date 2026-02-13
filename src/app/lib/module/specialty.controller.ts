36956d2550097fe105784aed03cf30e30150b690
AsadFahimTEC <fah485434@gmail.com>
2026-02-13 18:59:22 +0600

    refactoring create specialty api for image upload

--- src/app/lib/module/specialty.controller.ts
+++ src/app/lib/module/specialty.controller.ts
@@ -6,7 +6,14 @@ import { sendResponse } from '../../shared/sendResponse';
 
 const createSpecialty = catchAsync(
     async (req: Request, res: Response) => {
-        const payload = req.body;
+        console.log(req.body);
+        console.log(req.file);
+        // const payload = req.body;
+        const payload = {
+            ...req.body,
+            icon: req.file?.path
+        };
+        
         const result = await SpecialtyService.createSpecialty(payload);
         sendResponse(res, {
             httpStatusCode: 201,
--- src/app/lib/module/specialty/specialty.route.ts
+++ src/app/lib/module/specialty/specialty.route.ts
@@ -3,10 +3,14 @@ import { Router } from "express";
 import { SpecialtyController } from "../specialty.controller";
 import { checkAuth } from "../../../middleware/checkAuth";
 import { Role } from "../../../../generated/prisma/enums";
+import { validateRequest } from "../../../middleware/validateRequest";
+import { SpecialtyValidation } from "./specialty.validation";
+import { multerUpload } from "../../../../config/multer.config";
 
 const router = Router();
 
-router.post('/', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), SpecialtyController.createSpecialty);
+router.post('/', multerUpload.single("file"), validateRequest(SpecialtyValidation.createSpecialtyZodSchema),  SpecialtyController.createSpecialty);
+// router.post('/', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), validateRequest(SpecialtyValidation.createSpecialtyZodSchema),  SpecialtyController.createSpecialty);
 router.get('/', SpecialtyController.getAllSpecialties);
 router.delete('/:id', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), SpecialtyController.deleteSpecialty);
 
