7460f240b2cbae80fced999d999f6dfba02f9a0e
AsadFahimTEC <fah485434@gmail.com>
2026-02-13 21:55:53 +0600

    creating constructor for query builder

--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -1,4 +1,4 @@
-import { PrismaCountArgs, PrismaFindManyArgs } from "../interfaces/query.interface";
+import { IQueryConfig, IQueryParams, PrismaCountArgs, PrismaFindManyArgs, PrismaModelDelegate } from "../interfaces/query.interface";
 
 export class QueryBuilder<
     T,
@@ -14,4 +14,21 @@ export class QueryBuilder<
     private sortBy: string = 'createdAt';
     private sortOrder: 'asc' | 'desc' = 'desc';
     private selectFields: Record<string, boolean> | undefined;
+
+    constructor(
+        private model: PrismaModelDelegate,
+        private queryParams: IQueryParams,
+        private config: IQueryConfig
+    ) {
+        this.query = {
+            where: {},
+            include: {},
+            orderBy: {},
+            skip: 0,
+            take: 10,
+        };
+        this.countQuery = {
+            where: {},
+        }
+    }
 }
