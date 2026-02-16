e325f7e782e91de49f1e23ae82d604ddc69b87fa
AsadFahimTEC <fah485434@gmail.com>
2026-02-16 13:36:43 +0600

    creating payment webhook handler

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import path from "path";
 import { envVars } from "./config/env";
 import cors from "cors";
 import qs from "qs";
+import { PaymentController } from "./app/lib/module/payment/payment.controller";
 
 
 const app: Application = express();
@@ -19,10 +20,7 @@ app.set("query parser", (str: string) => qs.parse(str));
 app.set("view engine", "ejs");
 app.set("views", path.resolve(process.cwd(), `src/app/templates`))
 
-app.post("/webhook", express.raw({ type: "application/json" }), async (req: Request, res: Response) => {
-  console.log("Webhook received", req.body);
-  res.status(200).json({ received: true });
-})
+app.post("/webhook", express.raw({ type: "application/json" }), PaymentController.handleStripeWebhookEvent)
 
 app.use(cors({
   origin: [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
