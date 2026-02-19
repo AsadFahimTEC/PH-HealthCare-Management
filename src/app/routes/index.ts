05cfb3dd104262a60442534219c4eeccdcaa1cc9
AsadFahimTEC <fah485434@gmail.com>
2026-02-19 12:01:39 +0600

    testing update my profile route with postman

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -6,12 +6,14 @@ import { DoctorRoutes } from "../lib/module/doctor/doctor.route";
 import { scheduleRoutes } from "../lib/module/schedule/schedule.route";
 import { DoctorScheduleRoutes } from "../lib/module/doctorSchedule/doctorSchedule.route";
 import { AppointmentRoutes } from "../lib/module/appointment/appointment.route";
+import { PatientRoutes } from "../lib/module/patient/patient.routes";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoutes);
 router.use("/users", UserRoutes);
+router.use("/patients", PatientRoutes);
 router.use("/doctors", DoctorRoutes);
 router.use("/schedules", scheduleRoutes)
 router.use("/doctor-schedules", DoctorScheduleRoutes)
