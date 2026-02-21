0eccae1c7b86defd10a850389cf5fb66ed7e06d9
AsadFahimTEC <fah485434@gmail.com>
2026-02-21 12:12:19 +0600

    creating dashboard api

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -10,6 +10,8 @@ import { PatientRoutes } from "../lib/module/patient/patient.routes";
 import { PrescriptionRoutes } from "../lib/module/prescription/prescription.route";
 import { ReviewRoutes } from "../lib/module/review/review.route";
 import { AdminRoutes } from "../lib/module/admin/admin.route";
+import { PaymentRoutes } from "../lib/module/payment/payment.route";
+import { StatsRoutes } from "../lib/module/stats/stats.route";
 
 const router = Router();
 
@@ -24,5 +26,7 @@ router.use("/doctor-schedules", DoctorScheduleRoutes);
 router.use("/appointments", AppointmentRoutes);
 router.use("/prescriptions", PrescriptionRoutes);
 router.use("/reviews", ReviewRoutes);
+router.use("/stats", StatsRoutes);
+router.use("/payments", PaymentRoutes);
 
 export const IndexRoutes = router;
