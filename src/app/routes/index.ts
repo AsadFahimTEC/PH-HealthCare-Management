f5348dcc3a517b06648423155b218c4a7be39f68
AsadFahimTEC <fah485434@gmail.com>
2026-02-21 11:04:21 +0600

    completing update and delete api properly

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -7,6 +7,9 @@ import { scheduleRoutes } from "../lib/module/schedule/schedule.route";
 import { DoctorScheduleRoutes } from "../lib/module/doctorSchedule/doctorSchedule.route";
 import { AppointmentRoutes } from "../lib/module/appointment/appointment.route";
 import { PatientRoutes } from "../lib/module/patient/patient.routes";
+import { PrescriptionRoutes } from "../lib/module/prescription/prescription.route";
+import { ReviewRoutes } from "../lib/module/review/review.route";
+import { AdminRoutes } from "../lib/module/admin/admin.route";
 
 const router = Router();
 
@@ -15,8 +18,11 @@ router.use("/specialties", SpecialtyRoutes);
 router.use("/users", UserRoutes);
 router.use("/patients", PatientRoutes);
 router.use("/doctors", DoctorRoutes);
-router.use("/schedules", scheduleRoutes)
-router.use("/doctor-schedules", DoctorScheduleRoutes)
-router.use("/appointments", AppointmentRoutes)
+router.use("/admins", AdminRoutes)
+router.use("/schedules", scheduleRoutes);
+router.use("/doctor-schedules", DoctorScheduleRoutes);
+router.use("/appointments", AppointmentRoutes);
+router.use("/prescriptions", PrescriptionRoutes);
+router.use("/reviews", ReviewRoutes);
 
 export const IndexRoutes = router;
